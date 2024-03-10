51a9ff34ba996d690822fe0b59b423e3412c85ed
Alessandro Bonadei <alessandro.bonadei96@gmail.com>
2024-03-10 10:52:24 +0000

    answered third question

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
+    <button @click="toggleModal">Filter</button>
+    <modal name="input-modal" v-if="isModalOpen">
+      <div class="modal-content">
+        <form @submit.prevent="submitForm" v-if="isModalOpen">
+          <label for="term">Term:</label>
+          <input type="text" v-model="term" id="term" required>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </modal>
     <post v-for="post in posts" :key="post.id" :post="post"></post>
+    <p v-if="posts.length === 0">No posts found!</p>
   </div>
 </template>
 
@@ -12,16 +23,43 @@ export default {
 
   data() {
     return {
-      posts: []
+      posts: [],
+      term: '',
+      isModalOpen: false
+    }
+  },
+
+  methods: {
+    toggleModal() {
+      this.isModalOpen = !this.isModalOpen;
+    },
+
+    submitForm() {
+      console.log('Form submitted with term:', this.term);
+      fetch(`/api/posts/search?term=${encodeURIComponent(this.term)}`)
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+        })
+        .catch(error => {
+          console.error('Error fetching search results:', error);
+        });
+    },
+
+    fetchPosts() {
+      fetch('/api/posts')
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+        })
+        .catch(error => {
+          console.error('Error fetching posts:', error);
+        });
     }
   },
 
   mounted() {
-    fetch('/api/posts')
-    .then(response => response.json())
-    .then(data => {
-      this.posts = data;
-    })
+    this.fetchPosts();
   }
 }
 </script>
